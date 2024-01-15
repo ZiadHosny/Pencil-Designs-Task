83aa32911c4266881a56d1dcb1f68e7aaa258c7c
ziad <ziad.hosny@bitspire.ca>
2024-01-15 15:02:53 +0200

    add global style and mui dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Box, ThemeProvider, createTheme } from "@mui/material";
+
+
+// Mui Dark Theme
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 export const App: React.FC = () => {
     return (
-        <div>App</div>
+        // Mui Theme Provider
+        <ThemeProvider theme={darkTheme}>
+            <Box className='app'>
+                <div>App</div>
+            </Box >
+        </ThemeProvider>
     );
 };
